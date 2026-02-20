3b7f2807991dd163484ad3e99ad6bf3d59b613cf
PatrykPod <pat93p@gmail.com>
2026-02-20 22:04:31 +0100

    initial commit

---
+++ interactive-maps.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Interactive Maps
+ * Description: GPS-like map made from your graphic where you pin interactive points by your choice. Canvas API is used.
+ * Version: 1.0
+ * Author: Patryk Podgórny
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
