bb745a7754432e83c380a140c2fe86ef92548d88
PatrykPod <pat93p@gmail.com>
2026-03-09 23:20:15 +0100

    add map preview in admin

--- admin/views/map-view.php
+++ admin/views/map-view.php
@@ -4,5 +4,20 @@
 <div class="wrap">
     <h1>Map preview</h1>
 
-    <canvas id="myCanvas"></canvas>
+    <div style="border:1px solid #ccd0d4;">
+        <canvas id="myCanvas"></canvas>
+    </div>
+
+    <div class="map-controls">
+        <button id="zoomInButton">+</button>
+        <button id="zoomOutButton">−</button>
+    </div>
 </div>
+
+<style>
+    #myCanvas {
+        width: 100%;
+        border: 1px solid #ccd0d4;
+        display: block;
+    }
+</style>
